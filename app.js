403864757f56c66e66001a5baf729bf46ca1b86d
SAM <samop20799@gmail.com>
2023-02-06 22:20:32 +0530

    Encryption in database passwords

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const mongoDB = "mongodb://127.0.0.1:27017/userDB";
 mongoose.set('strictQuery', true);
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -19,10 +20,19 @@ mongoose.connect(mongoDB,(err)=>{
     }
 });
 
-const userSchema = {
+const {Schema} = mongoose;                         // we wrote proper way to describe a schema to use encryption in it
+
+const userSchema = new Schema({
     email: String,
     password: String
-};
+});
+
+const secret = "Thisisourlittlesecret.";                                           // we made our own key for encryption
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password']});       // this should be always above you mongoose model
+
+// here we used encrypt plugin and give our own key to addn encryption we only added encryption in password if we want to add more items just add that item name as string inside encryption field array
+// how encryption works is at .save it encrypts the data and at .find it decrypts the data so even you pass is encrypted if you log it inside you .find() you will get exact password without encryption
+// now new users once register in you database their passwords will be saved as long bin data no one can even understand what that item is
 
 const User = mongoose.model("user",userSchema);
 
