06728b3b820c7f1132cfe4f10aa4d0b557b8a81e
SAM <samop20799@gmail.com>
2023-02-08 10:26:40 +0530

    listning port updated for deploy in  app.js

--- app.js
+++ app.js
@@ -156,7 +156,7 @@ app.post("/login", function(req,res){
     });
 });
 
-
-app.listen(3000, function(){
-    console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+  console.log(`app listning on PORT ${port}`);
 });
